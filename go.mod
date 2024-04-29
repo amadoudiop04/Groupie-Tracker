a6500aec0aaa415d6d5df35b396142b2a316c501
syassine <yassine.sghaier@ynov.com>
2024-04-29 14:58:40 +0200

    first version of petit bac

---
+++ go.mod
@@ -0,0 +1,3 @@
+module petit_bac
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+var entries []map[string]string
+
+func main() {
+    http.HandleFunc("/", handleHome)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+    log.Println("Serveur démarré sur :8080")
+    go func() {
+        log.Fatal(http.ListenAndServe(":8080", nil))
+    }()
+    
+    fmt.Println("Appuyez sur Entrée pour quitter...")
+    fmt.Scanln()
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        r.ParseForm()
+
+        artiste := r.Form.Get("artiste")
+        album := r.Form.Get("album")
+        groupe := r.Form.Get("groupe")
+        instrument := r.Form.Get("instrument")
+        featuring := r.Form.Get("featuring")
+
+        fmt.Println("Nouvelle entrée ajoutée :")
+        fmt.Println("Artiste:", artiste)
+        fmt.Println("Album:", album)
+        fmt.Println("Groupe de musique:", groupe)
+        fmt.Println("Instrument de musique:", instrument)
+        fmt.Println("Featuring:", featuring)
+    } else {
+        http.ServeFile(w, r, "index.html")
+    }
+}
+
+
+
+
+// var categories = []string{"Artiste", "Album", "Groupe de musique", "Instrument de musique", "Featuring"}
+
+// func contains(s []string, e string) bool {
+//     for _, a := range s {
+//         if a == e {
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+// func isUnique(s []string, e string) bool {
+//     count := 0
+//     for _, a := range s {
+//         if a == e {
+//             count++
+//         }
+//     }
+//     return count == 1
+// }
+
+
+
+// func bac() {
+    
+
+   
+// }
+// rand.Seed(time.Now().UnixNano())
+//     letters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//     randomLetter := string(letters[rand.Intn(len(letters))])
+
+//     fmt.Println(randomLetter)
